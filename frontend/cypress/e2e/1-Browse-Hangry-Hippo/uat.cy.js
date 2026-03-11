e3fb1c3644b60dfdc2b00432d564eeb0ac7cdb9a
yarndresses <yarndresses@gmail.com>
2026-03-11 14:12:53 -0400

    lesson 7 changes

--- frontend/cypress/e2e/1-Browse-Hangry-Hippo/uat.cy.js
+++ frontend/cypress/e2e/1-Browse-Hangry-Hippo/uat.cy.js
@@ -3,9 +3,15 @@ describe('Shows Appeteasers', () => {
     cy.visit('https://hangryhippo.quantic.host/');
   });
   it('selects Handhelds', () => {
-    //TODO: Add a test to check Handhelds
+    cy.contains('Handhelds').click();
+    cy.contains('Cheese Burger');
+    cy.contains('Fajita Tacos');
   });
   it('selects Appeteasers', () => {
-    //TODO: Add a test to check Appeteasers
+    cy.contains('Appeteasers').click();
+    cy.contains('Tater Tots');
+    cy.contains('Buffalo Wings');
+    cy.contains('Cheese Burger').should('not.exist');
+    cy.contains('Fajita Tacos').should('not.exist');
   });
 });
